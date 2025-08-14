38c840f79c67ae9ca6ad1272852adc45479029e6
Fomafix <fomafix@googlemail.com>
2025-08-14 17:04:34 +0000

    Use private property instead of global variable
    Change-Id: Iec201aa11f7fda5c59571b1321bdd2c2f82ac1b9

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -7,15 +7,18 @@ use Parser;
 class Hooks implements
 	\MediaWiki\Hook\ParserFirstCallInitHook
 {
+	private CategoryTests $categoryTests;
+
+	public function __construct() {
+		$this->categoryTests = new CategoryTests();
+	}
+
 	/**
 	 * @param Parser $parser
 	 */
 	public function onParserFirstCallInit( $parser ) {
-		global $wgExtCategoryTests;
-
-		$wgExtCategoryTests = new CategoryTests();
-		$parser->setFunctionHook( 'ifcategory', [ &$wgExtCategoryTests, 'ifcategory' ] );
-		$parser->setFunctionHook( 'ifnocategories', [ &$wgExtCategoryTests, 'ifnocategories' ] );
-		$parser->setFunctionHook( 'switchcategory', [ &$wgExtCategoryTests, 'switchcategory' ] );
+		$parser->setFunctionHook( 'ifcategory', [ $this->categoryTests, 'ifcategory' ] );
+		$parser->setFunctionHook( 'ifnocategories', [ $this->categoryTests, 'ifnocategories' ] );
+		$parser->setFunctionHook( 'switchcategory', [ $this->categoryTests, 'switchcategory' ] );
 	}
 }
